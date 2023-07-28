11968b227f9cc99f9e5a5b7fdb99e3bf142963a1
ibrahim AL_Timama <112539184+sudo0ibrahim@users.noreply.github.com>
2023-07-28 17:38:37 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void encode(char c, int key){
+    
+    int save = int(c) + key;
+    
+    if(save <= 122){
+        cout<<char(save);
+    }
+    else{
+        
+        cout<<char((save-122)+96);
+    }
+
+}
+
+void decode(char c, int key){
+    
+    int save = int(c) - key;
+    
+    if(save >= 97){
+        
+        cout<<char(save);
+    }
+    else{
+        cout<<char(122-(96-save));
+    }
+    
+}
+
+int main()
+{
+    
+    cout<<"                                     /$$          "<<endl;
+    cout<<"                                    | $$          "<<endl;
+    cout<<"  /$$$$$$  /$$$$$$$   /$$$$$$$  /$$$$$$$  /$$$$$$ "<<endl;
+    cout<<" /$$__  $$| $$__  $$ /$$_____/ /$$__  $$ /$$__  $$"<<endl;
+    cout<<"| $$$$$$$$| $$  \\ $$| $$      | $$  | $$| $$$$$$$$"<<endl;
+    cout<<"| $$_____/| $$  | $$| $$      | $$  | $$| $$_____/"<<endl;
+    cout<<"|  $$$$$$$| $$  | $$|  $$$$$$$|  $$$$$$$|  $$$$$$$"<<endl;
+    cout<<" \\_______/|__/  |__/ \\_______/ \\_______/ \\_______/"<<endl;
+    cout<<"[twitter: @ibrahim15793496] || [instagram: ibrahim_1arabic]"<<endl;
+
+    while(true){
+
+        char q;
+        int key;
+        string word;
+
+        cout<<"\n";
+        cout<<"encode[e] decode[d]: ";
+        cin>>q;
+        cout<<"word: ";
+        cin>>word;
+        cout<<"key: ";
+        cin>>key;
+        
+        if(q == 'e'){
+            
+            for(int i =0; i< word.length(); i++){
+                
+                encode(word[i], key);
+            }
+        }
+        else if(q == 'd'){
+            
+            for(int i =0; i < word.length(); i++){
+                
+                decode(word[i], key);
+                
+            }
+        }
+
+    }
+    return 0;
+}
